62545dfe517dc5d556d81dd8c688b9d38aee8830
sei-12 <sei.t11@icloud.com>
2024-12-20 05:29:54 +0900

    update

--- skin.rb
+++ skin.rb
@@ -3,7 +3,7 @@ require "formula"
 class Skin < Formula
   desc "タグで検索を行うブックマークマネージャー"
   url "https://github.com/sei-12/skin/releases/tag/v0.1.5"
-  sha256 "cf7756413cb6d8a4531f12f4d1ca4e3f4ecb12893f1cd676083ffc93e95b0ce4"
+  sha256 "5c2beb6629de908907685a5d9dc2f40131b4dacce1f5fd88d06af17dbdb380a8"
   license "MIT" # プロジェクトのライセンスを記載
   def install
     bin.install "skin" # バイナリの名前に合わせる
