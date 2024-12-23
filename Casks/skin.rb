6dca159dc8e2668f6dbb73bff2bc8a60f70242d7
sei-12 <sei.t11@icloud.com>
2024-12-23 09:02:02 +0900

    update version

--- Casks/skin.rb
+++ Casks/skin.rb
@@ -1,7 +1,7 @@
 cask "skin" do
-  version "0.1.7"
-  sha256 "612bbd7ff2126771edf110132212f3eed56272ac113f6c8165a7385d62d9c953"
-  url "https://github.com/sei-12/skin/releases/download/app-v0.1.7/skin_0.1.7_aarch64.dmg"
+  version "0.1.8"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/sei-12/skin/releases/download/app-v0.1.8/skin_0.1.8_aarch64.dmg"
   name "skin"
   desc "A sample application"
   homepage "https://github.com/sei-12/skin"
