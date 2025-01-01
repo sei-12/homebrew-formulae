d6d18cb4d43dfd764b7c7b32f57416689a342db7
sei-12 <>
2025-01-01 05:15:12 +0000

    update sample-tauri-app to version

---
+++ Casks/sample-tauri-app.rb
@@ -0,0 +1,14 @@
+    cask "sample-tauri-app" do 
+      version "0.1.4"
+      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      url "https://github.com/sei-12/sample-tauri-app/releases/download/app-v0.1.4/sample-tauri-app.dmg"
+      name "sample-tauri-app"
+      desc "A sample application"
+      homepage "https://github.com/sei-12/sample-tauri-app"
+
+      app "sample-tauri-app.app"
+
+      zap trash: [
+        "~/Library/Application Support/com.sample-tauri-app.app",
+      ]
+    end
