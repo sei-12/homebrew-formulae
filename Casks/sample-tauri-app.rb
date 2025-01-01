589f297770ff035ee4634119be5ee3d5d9b05946
sei-12 <>
2025-01-01 05:21:36 +0000

    update sample-tauri-app to version

--- Casks/sample-tauri-app.rb
+++ Casks/sample-tauri-app.rb
@@ -1,7 +1,7 @@
     cask "sample-tauri-app" do 
-      version "0.1.4"
+      version "0.1.5"
       sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-      url "https://github.com/sei-12/sample-tauri-app/releases/download/app-v0.1.4/sample-tauri-app.dmg"
+      url "https://github.com/sei-12/sample-tauri-app/releases/download/app-v0.1.5/sample-tauri-app0.1.5_aarch64.dmg"
       name "sample-tauri-app"
       desc "A sample application"
       homepage "https://github.com/sei-12/sample-tauri-app"
