562af7511cbe8939931e4da5356e833dc0b469df
sei-12 <>
2025-02-06 13:47:55 +0000

    Update version to 1.0.12 for skin

--- Casks/skin.rb
+++ Casks/skin.rb
@@ -1,7 +1,7 @@
     cask "skin" do 
-      version "1.0.11"
-      sha256 "71ca134291c5a166e781211e9952615c9caf4c3fdccd5de0f722682bc4e8a59e"
-      url "https://github.com/sei-12/skin/releases/download/app-v1.0.11/skin_1.0.11_aarch64.dmg"
+      version "1.0.12"
+      sha256 "c164e2441bd63ca5e7a4984c260a670e6791e684504b05faa8d328688034e6b1"
+      url "https://github.com/sei-12/skin/releases/download/app-v1.0.12/skin_1.0.12_aarch64.dmg"
       name "skin"
       desc "A sample application"
       homepage "https://github.com/sei-12/skin"
